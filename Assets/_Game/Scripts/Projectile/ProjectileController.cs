13d247562508be70b5c9a4d428d9b366ffcf56ae
muhammed <muhammed68jg@gmail.com>
2024-09-12 19:21:05 +0300

    WeaponUpdate
    tabanca eklendi , ateş etme eklendi .

--- Assets/_Game/Scripts/Projectile/ProjectileController.cs
+++ Assets/_Game/Scripts/Projectile/ProjectileController.cs
@@ -1,12 +1,22 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
 public class ProjectileController : MonoBehaviour
 {
-    
-    void FixedUpdate()
+    public float speed = 8f;
+    public float distance = 10f; // Merminin gideceði mesafe
+    public Ease easeType = Ease.Linear; // Ease tipi
+
+
+    void Start()
     {
-        transform.position += transform.forward * Time.fixedDeltaTime * 8f;
+        // Merminin hedef pozisyonunu hesaplayalým (mesafe kadar ileri)
+        Vector3 targetPosition = transform.position + transform.forward * distance;
+
+        // DoTween ile hareket ettiriyoruz
+        transform.DOMove(targetPosition, distance / speed) // Hareket süresi = mesafe / hýz
+            .SetEase(easeType); // Ease tipini burada ekliyoruz
     }
 }
